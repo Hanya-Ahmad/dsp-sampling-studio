771197e1b616efb3f55faf6cbc654f118bc13716
هانيااحمدسامىعبدالعظ <@DESKTOP-E8F3FRI>
2022-10-20 14:02:51 +0200

    added noise to csv file

--- staticcsv.py
+++ staticcsv.py
@@ -19,31 +19,34 @@ import mpld3
 from matplotlib.animation import FuncAnimation
 from tkinter import HORIZONTAL, Menu
 from turtle import color, width
-mpl.pyplot.ion()
 
 
 menus= option_menu(menu_title="Select a page.",options=["Sample","Compose"],default_index=0,orientation=HORIZONTAL)
-
+noise_checkbox=st.checkbox("Add noise",value=False) 
 
 def generate ():
     global uploaded_file
     uploaded_file = st.file_uploader(label="Upload your CSV file", type=['csv', 'xlsx'])
 
-
     global df
     if uploaded_file is not None:
-
+        
         try:
             df = pd.read_csv(uploaded_file)
+            
+            
         except Exception as e:
             df = pd.read_excel(uploaded_file)
 
 
     try:
+        
         interactive_plot(df)
 
     except Exception as e:
         st.write("You haven't uploaded a signal yet")  
+        print("You haven't uploaded a signal yet")
+        print(e)
             
 
            
@@ -53,23 +56,38 @@ def generate ():
   
   
 def interactive_plot(dataframe):
-    
-      time= df['time'].tolist()
-      amplitude = df['amplitude'].tolist()
-      col = st.color_picker('Select a plot color')
-    
-      plot = px.line(dataframe,x=time,y=amplitude,width=800,height=600,title=uploaded_file.name,range_x=[9, 10.2],range_y=[-1,1.5], template="plotly_dark")
-      plot.update_traces(marker=dict(color=col))
+    snr_db=0
 
-      plot.update_xaxes(title_text='Time')
-      plot.update_yaxes(title_text='amplitude')
+    if(noise_checkbox):
+        snr_db=st.number_input("SNR level",value=0,min_value=0,max_value=120,step=5)
+    time= df['time'].tolist()
+    amplitude = df['amplitude'].tolist()
+    col = st.color_picker('Select a plot color')
+    mean=df['amplitude'].mean()
+    std_deviation=df['amplitude'].std()
+    power=df['amplitude']**2
+    signal_average_power=np.mean(power)
+    signal_averagePower_db=10*np.log10(signal_average_power)
+            
+    noise_db=signal_averagePower_db-snr_db
+    noise_watts=10**(noise_db/10)
+    mean_noise=0
+    noise=np.random.normal(mean_noise,np.sqrt(noise_watts),len(df['amplitude']))
+    noise_signal=df['amplitude']+noise
+    if(noise_checkbox):
+        plot = px.line(dataframe,x=time,y=noise_signal,width=800,height=600,title=uploaded_file.name,range_x=[9, 10.2],range_y=[-1,1.5], template="plotly_dark")
+    else:
+        plot = px.line(dataframe,x=time,y=amplitude,width=800,height=600,title=uploaded_file.name,range_x=[9, 10.2],range_y=[-1,1.5], template="plotly_dark")
+    plot.update_traces(marker=dict(color=col))
+    print('CHECKBOX: ', noise_checkbox)
+    plot.update_xaxes(title_text='Time')
+    plot.update_yaxes(title_text='amplitude')
      
      
-      st.plotly_chart(plot)
+    st.plotly_chart(plot)
      
 
 
 
 if menus=="Sample":
-    generate(),
-    
+    generate()
