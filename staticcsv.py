b625705208316111b886ba7df3bf1d2dd65daa75
ezzat223 <ezzat22hegazy@gmail.com>
2022-10-20 17:08:02 +0200

    blue

--- staticcsv.py
+++ staticcsv.py
@@ -22,15 +22,29 @@ from tkinter import HORIZONTAL, Menu
 from turtle import color, width
 mpl.pyplot.ion()
 
+
+
+st.set_page_config(
+    
+    page_title="Sampling Studio.",
+    page_icon="tv",
+    layout='centered'
+    
+)
+
+st.title("Sampling Sudio Web App.")
+
 menus= option_menu(menu_title="Select a page.",options=["Sample","Compose"],default_index=0,orientation=HORIZONTAL)
-noise_checkbox=st.checkbox("Add noise",value=False) 
+ 
 
 def generate ():
     global uploaded_file
     uploaded_file = st.file_uploader(label="Upload your CSV file", type=['csv', 'xlsx'])
 
     global df
+    global noise_checkbox
     if uploaded_file is not None:
+        noise_checkbox=st.checkbox("Add noise",value=False)
         
         try:
             df = pd.read_csv(uploaded_file)
@@ -63,7 +77,7 @@ def interactive_plot(dataframe):
         snr_db=st.number_input("SNR level",value=0,min_value=0,max_value=120,step=5)
     time= df['time'].tolist()
     amplitude = df['amplitude'].tolist()
-    col = st.color_picker('Select a plot color')
+    col = st.color_picker('Select a plot color','#0827F5')
     mean=df['amplitude'].mean()
     std_deviation=df['amplitude'].std()
     power=df['amplitude']**2
