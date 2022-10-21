721199128c427cd5161ef9da7b4d245dfb3bb9e9
هانيااحمدسامىعبدالعظ <@DESKTOP-E8F3FRI>
2022-10-21 17:44:04 +0200

    add waves to same plot completed

--- staticsignalgenerator.py
+++ staticsignalgenerator.py
@@ -17,28 +17,47 @@ import streamlit.components.v1 as components
 
 st.title("Sampling Studio")
 st.sidebar.title("Options")
-"st.session_state object:", st.session_state
-if 'added_signals' not in st.session_state:
-    	st.session_state.added_signals = []
-
-
-# org_freq=st.sidebar.slider("Frequency",min_value=0,max_value=100,value=20,step=5)
-noise_checkbox=st.sidebar.checkbox("Add noise",value=False) 
-sampling_checkbox=st.sidebar.checkbox("Sampling", value=False)
-if(sampling_checkbox):
-    samp_freq=st.sidebar.slider("Sampling Frequency",min_value=1,max_value=100,value=20)
 
 #wave variables
 frequency = st.sidebar.slider('frequency',1, 10, 1, 1)  # freq (Hz)
 amplitude=st.sidebar.slider('amplitude',1,10,1,1)
 time= np.linspace(0, 3, 1200) #time steps
 sine = amplitude * np.sin(2 * np.pi * frequency* time) # sine wave 
-
-
-
 snr_db=0
+noise_checkbox=st.sidebar.checkbox("Add noise",value=False) 
+sampling_checkbox=st.sidebar.checkbox("Sampling", value=False)
+adding_waves_checkbox=st.sidebar.checkbox("adding waves", value=False)
+
+#show snr slider when noise checkbox is true
 if noise_checkbox:
      snr_db=st.sidebar.number_input("SNR level",value=0,min_value=0,max_value=120,step=5)
+
+#noise variables
+power=sine**2
+signal_average_power=np.mean(power)
+signal_averagePower_db=10*np.log10(signal_average_power)
+noise_db=signal_averagePower_db-snr_db
+noise_watts=10**(noise_db/10)
+mean_noise=0
+noise=np.random.normal(mean_noise,np.sqrt(noise_watts),len(sine))
+noise_signal=sine+noise
+
+#show fs slider when sampling checkbox is true
+if(sampling_checkbox):
+    samp_freq=st.sidebar.slider("Sampling Frequency",min_value=1,max_value=100,value=20)
+
+#if session state has no 'added signals' object then create one 
+if 'added_signals' not in st.session_state:
+    st.session_state['added_signals'] = []
+    
+    #if noise checkbox is true then plot the main signal with noise
+    if(noise_checkbox):
+        st.session_state.added_signals = [{'name':'main','x':time,'y':noise_signal}]
+
+    #else plot the main signal without noise
+    else:
+      st.session_state.added_signals = [{'name':'main','x':time,'y':sine}] 
+
 
 st.markdown("""
 <tr>
@@ -62,44 +81,19 @@ st.markdown("""
 
 st.write('''### Sine Wave''')
 
-st.write("First")
-st.write(st.session_state)
-
-
-#power signal 
-power=sine**2
-signal_average_power=np.mean(power)
-signal_averagePower_db=10*np.log10(signal_average_power)
-noise_db=signal_averagePower_db-snr_db
-noise_watts=10**(noise_db/10)
-
-mean_noise=0
-noise=np.random.normal(mean_noise,np.sqrt(noise_watts),len(sine))
-noise_signal=sine+noise
-
-if(noise_checkbox):
-    st.session_state.added_signals = [{'name':'main','x':time,'y':noise_signal}]
-else:
-   st.session_state.added_signals = [{'name':'main','x':time,'y':sine}] 
-
-#add a signal
-
+# function to add a signal
 def add_signal(label,x,y):
     st.session_state.added_signals.append({'name':label, 'x':x, 'y':y})
     
 
-#remove a signal
+#function to remove a signal
 def remove_signal(deleted_name):
     for i in range(len(st.session_state.added_signals)):
         if st.session_state.added_signals[i]['name']==deleted_name:
             del st.session_state.added_signals[i]
             break
 
-adding_waves_checkbox=st.sidebar.checkbox("adding waves", value=False)
-
-    #add_signal(added_label,time,added_sine)
-# Finding amplitude at each time
-#sampling variables
+#sampling code
 if (sampling_checkbox):
     T=1/samp_freq 
     n=np.arange(0,3/T)
@@ -112,70 +106,60 @@ if (sampling_checkbox):
         sampled_amplitude=amplitude*np.sin(2 * np.pi * frequency * nT )
 
 
-# Finally displaying the plot
-#plt.show()
+#helper function
 def cm_to_inch(value):
     return value/2.54
 
+#change plot size
 fig=plt.figure(figsize=(cm_to_inch(60),cm_to_inch(45)))
 
-
+#set plot parameters
 plt.subplot(211)
-
-# Setting x axis label for the plot
 plt.xlabel('Time'+ r'$\rightarrow$')
-
-# Setting y axis label for the plot
 plt.ylabel('Sin(time) '+ r'$\rightarrow$')
-
-# Showing grid
 plt.grid()
-
-# Highlighting axis at x=0 and y=0
 plt.axhline(y=0, color='k')
 plt.axvline(x=0, color='k')
 
-# Plotting time vs amplitude using plot function from pyplot
-if noise_checkbox:
+# if noise checkbox is clicked plot noise signal against time
+if (noise_checkbox):
     plt.plot(time, noise_signal,label='signal with noise')
 else:
         plt.plot(time, sine,label='signal')
 plt.legend(fontsize=20, loc='upper right')
 
-# if(adding_waves_checkbox):
-# plt.subplot(212)
-#plt.plot(time, final_added_wave,label='added signals') '''
-
+#execute sampling function if sampling checkbox is true
 if(sampling_checkbox):
     plt.subplot(212)
     plt.xlabel('Time'+ r'$\rightarrow$')
- #Setting y axis label for the plot
     plt.ylabel('Sin(time) '+ r'$\rightarrow$')
-        # Showing grid
     plt.grid()
-
-    # Highlighting axis at x=0 and y=0
     plt.axhline(y=0, color='k')
     plt.axvline(x=0, color='k')
     plt.plot(nT,sampled_amplitude,'r*',label='sampled points')
     plt.plot(nT,sampled_amplitude,label='reconstructed wave')
     plt.legend(fontsize=20, loc='upper right')
-#print(st.session_state.added_signals)
+
+#execute adding wave function if adding wave checkbox is true 
 if(adding_waves_checkbox):
     added_frequency = st.sidebar.slider('frequency for added wave',1, 10, 1, 1)  # freq (Hz)
     added_amplitude=st.sidebar.slider('amplitude for added wave',1,10,1,1)
     added_sine=added_amplitude*np.sin(2*np.pi*added_frequency*time)
     added_label=str(np.random.normal(0,100))
     add_wave_button=st.sidebar.button("Add Wave")
+
+    #call the add_signal function when button is clicked
     if(add_wave_button):
         add_signal(added_label,time,added_sine)
+
+#loop over each item in added_signals and plot them all on the same plot   
 for signal in st.session_state.added_signals:
     plt.subplot(211)
     plt.plot(signal['x'], signal['y'],
             label=signal['name'])
     plt.legend()
    
-st.write(st.session_state)
+
 
 
 st.pyplot(fig)
