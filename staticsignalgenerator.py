a3e67e107d79df769e0a77910c7f5eff3357da0f
aishawaziry <81515483+aishawaziry@users.noreply.github.com>
2022-10-22 13:47:41 +0200

    Update staticsignalgenerator.py

--- staticsignalgenerator.py
+++ staticsignalgenerator.py
@@ -25,12 +25,11 @@ time= np.linspace(0, 3, 1200) #time steps
 sine = amplitude * np.sin(2 * np.pi * frequency* time) # sine wave 
 snr_db=0
 noise_checkbox=st.sidebar.checkbox("Add noise",value=False) 
-sampling_checkbox=st.sidebar.checkbox("Sampling", value=False)
-adding_waves_checkbox=st.sidebar.checkbox("adding waves", value=False)
-
 #show snr slider when noise checkbox is true
 if noise_checkbox:
      snr_db=st.sidebar.number_input("SNR level",value=0,min_value=0,max_value=120,step=5)
+sampling_checkbox=st.sidebar.checkbox("Sampling", value=False)
+
 
 #noise variables
 power=sine**2
@@ -44,8 +43,10 @@ noise_signal=sine+noise
 
 #show fs slider when sampling checkbox is true
 if(sampling_checkbox):
-    reconstruct_checkbox=st.sidebar.checkbox("reconstruct Sampling Signal", value=False)
     samp_freq=st.sidebar.slider("Sampling Frequency",min_value=1,max_value=100,value=20)
+    reconstruct_checkbox=st.sidebar.checkbox("reconstruct Sampling Signal", value=False)
+    
+adding_waves_checkbox=st.sidebar.checkbox("adding waves", value=False)
 
 #if session state has no 'added signals' object then create one 
 if 'added_signals' not in st.session_state:
@@ -60,17 +61,6 @@ if 'added_signals' not in st.session_state:
       st.session_state.added_signals = [{'name':'main','x':time,'y':sine}] 
 
 
-st.markdown("""
-<tr>
-""",True)
-st.markdown("""
-<style>
-.css-d1b1ld.edgvbvh6
-{
- visibility:hidden;
-}
-</style>
-""",unsafe_allow_html=True)
 st.markdown("""
 <style>
 .css-12gp8ed.eknhn3m4
@@ -119,7 +109,9 @@ def sinc_interp(nt_array, sampled_amplitude , time):
     sincM = np.tile(time, (len(sampled_amplitude), 1)) - np.tile(sampled_amplitude[:, np.newaxis], (1, len(time)))
     yNew = np.dot(nt_array, np.sinc(sincM/T))
     plt.subplot(212)
-    plt.plot(time,yNew,'r-')
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.plot(time,yNew,'r-',label='Reconstructed wave')
 
 #helper function
 def cm_to_inch(value):
@@ -130,38 +122,40 @@ fig=plt.figure(figsize=(cm_to_inch(60),cm_to_inch(45)))
 
 #set plot parameters
 plt.subplot(211)
-plt.xlabel('Time'+ r'$\rightarrow$')
-plt.ylabel('Sin(time) '+ r'$\rightarrow$')
+plt.xlabel('Time'+ r'$\rightarrow$',fontsize=20)
+plt.ylabel('Sin(time) '+ r'$\rightarrow$',fontsize=20)
 plt.grid()
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
 plt.axhline(y=0, color='k')
 plt.axvline(x=0, color='k')
 
 # if noise checkbox is clicked plot noise signal against time
 if (noise_checkbox):
     plt.plot(time, noise_signal,label='signal with noise')
-else:
-        plt.plot(time, sine,label='signal')
-plt.legend(fontsize=20, loc='upper right')
+    plt.legend(fontsize=20, loc='upper right')
+
+# else:
+#         plt.plot(time, sine,label='signal')
+#         plt.legend(fontsize=20, loc='upper right')
 
 #execute sampling function if sampling checkbox is true
 if(sampling_checkbox):
     if reconstruct_checkbox:
         sinc_interp( sampled_amplitude,nT_array , time)
     plt.subplot(212)
-    plt.xlabel('Time'+ r'$\rightarrow$')
+    plt.xlabel('Time'+ r'$\rightarrow$',fontsize=20)
  #Setting y axis label for the plot
-    plt.ylabel('Sin(time) '+ r'$\rightarrow$')
+    plt.ylabel('Sin(time) '+ r'$\rightarrow$',fontsize=20)
         # Showing grid
     plt.grid()
-
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
     # Highlighting axis at x=0 and y=0
     plt.axhline(y=0, color='k')
     plt.axvline(x=0, color='k')
-    plt.stem(nT,sampled_amplitude,'b',label='sampled points',linefmt='b')
-    # if reconstruct_checkbox:
-    #      plt.plot(nT,wsinterp(),'k',label='reconstructed wave')
-    # plt.plot(nT,sampled_amplitude,label='reconstructed wave')
-    plt.legend(fontsize=20, loc='upper right')
+    plt.stem(nT,sampled_amplitude,'b',label='sampled points',linefmt='b',basefmt=" ")
+    plt.legend(fontsize=16, loc='upper right')
 
 #execute adding wave function if adding wave checkbox is true 
 if(adding_waves_checkbox):
@@ -180,7 +174,7 @@ for signal in st.session_state.added_signals:
     plt.subplot(211)
     plt.plot(signal['x'], signal['y'],
             label=signal['name'])
-    plt.legend()
+    plt.legend(fontsize=16)
    
 
 
